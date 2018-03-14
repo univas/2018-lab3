7db12de27ef1acccd7f26d7d903bf0972ea56e60
Roberto Rocha <rrocha.roberto@gmail.com>
2018-03-14 20:27:45 -0300

    Defining the first class and its objects.

---
+++ Aula6/src/br/edu/univas/si3/lab3/Data.java
@@ -0,0 +1,12 @@
+package br.edu.univas.si3.lab3;
+
+public class Data {
+
+	//atributos
+	int dia;
+	int mes;
+	
+	void imprimir() {
+		System.out.println("Data:" + dia + "/" + mes);
+	}
+}
---
+++ Aula6/src/br/edu/univas/si3/lab3/Runner.java
@@ -0,0 +1,32 @@
+package br.edu.univas.si3.lab3;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		//declaração das variáveis dos objetos
+		Data ptr1;
+		Data ptr2;
+		Data ptr3;
+		
+		//alocar os objetos na memória
+		ptr1 = new Data();
+		ptr2 = new Data();
+		ptr3 = null;
+
+		//acessar os atributos dos objetos
+		ptr1.dia = 5;
+		ptr1.mes = 11;
+		
+		ptr2.dia = 14;
+		ptr2.mes = 3;
+		
+		//imprimindo os atributos
+		System.out.println("Data inicial: " + ptr1.dia + "/" + ptr1.mes);
+		System.out.println("Data final: " + ptr2.dia + "/" + ptr2.mes);
+		
+		//acessando os métodos - chamando o método 'imprimir' de cada objeto
+		ptr1.imprimir();		
+		ptr2.imprimir();
+	}
+}

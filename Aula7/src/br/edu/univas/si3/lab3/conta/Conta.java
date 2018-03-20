bda6a1ffe5e69902a7554c642a6a6a27ccba5cdf
Roberto Rocha <rrocha.roberto@gmail.com>
2018-03-19 22:16:53 -0300

    Exemplo de conta bancaria.

---
+++ Aula7/src/br/edu/univas/si3/lab3/conta/Conta.java
@@ -0,0 +1,20 @@
+package br.edu.univas.si3.lab3.conta;
+
+public class Conta {
+
+	//exercício 1:
+	//criar um atributo inteiro para o saldo e para o número da conta
+	//criar um construtor para inicializar os atributos
+	//criar um método para retornar o saldo
+	//criar um método para retornar o número
+	//criar um método para depositar
+	//criar um método para sacar
+	//criar um método para transferir para outra conta
+	
+	//exercício 2:
+	//criar uma classe de teste para verificar se os métodos estão corretos.
+	
+	//atividade 3:
+	//criar uma classe de gerente de contas
+	//
+}

73270f11bf21814dc7eb7da8acf6373ae21b7104
Roberto Rocha <rrocha.roberto@gmail.com>
2018-03-19 22:18:28 -0300

    Construtor e visibilidade.

--- Aula6/src/br/edu/univas/si3/lab3/Data.java
+++ Aula6/src/br/edu/univas/si3/lab3/Data.java
@@ -3,10 +3,26 @@ package br.edu.univas.si3.lab3;
 public class Data {
 
 	//atributos
-	int dia;
-	int mes;
+	private int dia;
+	public int mes;
 	
-	void imprimir() {
+	//construtor - se parece com um método, porém não tem tipo de retorno
+	//executa apenas inicialização dos atributos
+	public Data(int novoValorDoDia, int novoValorDoMes) {
+		dia = novoValorDoDia;
+		mes = novoValorDoMes;
+	}
+	
+	public Data(int soValorDoMes) {
+		mes = soValorDoMes;
+	}
+
+	//construtor default
+	public Data() {
+		
+	}
+	
+	public void imprimir() {
 		System.out.println("Data:" + dia + "/" + mes);
 	}
 }
--- Aula6/src/br/edu/univas/si3/lab3/Runner.java
+++ Aula6/src/br/edu/univas/si3/lab3/Runner.java
@@ -10,20 +10,20 @@ public class Runner {
 		Data ptr3;
 		
 		//alocar os objetos na memória
-		ptr1 = new Data();
+		ptr1 = new Data(5, 11);
 		ptr2 = new Data();
 		ptr3 = null;
 
 		//acessar os atributos dos objetos
-		ptr1.dia = 5;
-		ptr1.mes = 11;
-		
-		ptr2.dia = 14;
+//		ptr1.dia = 5;
+//		ptr1.mes = 11;
+//		
+//		ptr2.dia = 14;//não pode por causa do private
 		ptr2.mes = 3;
 		
 		//imprimindo os atributos
-		System.out.println("Data inicial: " + ptr1.dia + "/" + ptr1.mes);
-		System.out.println("Data final: " + ptr2.dia + "/" + ptr2.mes);
+//		System.out.println("Data inicial: " + ptr1.dia + "/" + ptr1.mes);
+//		System.out.println("Data final: " + ptr2.dia + "/" + ptr2.mes);
 		
 		//acessando os métodos - chamando o método 'imprimir' de cada objeto
 		ptr1.imprimir();		

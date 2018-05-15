25297876575a133ab498e29d859612d98194b9dd
Roberto Rocha <rrocha.roberto@gmail.com>
2018-05-14 22:14:04 -0300

    Criando outras interfaces.

---
+++ Aula9/src/br/edu/univas/si3/lab3/conta/Cancelavel.java
@@ -0,0 +1,6 @@
+package br.edu.univas.si3.lab3.conta;
+
+public interface Cancelavel {
+
+	public boolean cancelar();
+}
--- Aula9/src/br/edu/univas/si3/lab3/conta/ContaSalario.java
+++ Aula9/src/br/edu/univas/si3/lab3/conta/ContaSalario.java
@@ -1,8 +1,26 @@
 package br.edu.univas.si3.lab3.conta;
 
-public class ContaSalario extends ContaCorrente {
+public class ContaSalario extends ContaCorrente implements Cancelavel, Espelhavel {
 
 	public ContaSalario(int numero, String cliente) {
 		super(numero, cliente, 0f);
 	}
+	
+	@Override
+	public boolean cancelar() {
+		if(getSaldo() == 0) {
+			return true;
+		}
+		//TODO: registrar um movimento de cancelamento.
+		return false;
+	}
+	
+	@Override
+	public void espelhar(ContaSalario destino) {
+		float saldoAtual = super.getSaldo();
+		super.sacar(saldoAtual);
+		destino.depositar(saldoAtual);
+		this.cancelar();//não consigo fazer super.cancelar();
+	}
+	
 }
